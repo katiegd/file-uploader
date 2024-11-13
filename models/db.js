31c51fb9064d6f6073fd53806eeea0869fde393c
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-11-13 16:59:26 -0500

    remove local db path for deployment.

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.RENDER_DB_URL,
+  connectionString: process.env.RENDER_DB_URL,
 });
 
 module.exports = pool;
